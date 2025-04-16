d4f4df4cdfbc0a7b3c4db82039351dff78a0f7eb
bonhoya <imhoya0407@naver.com>
2025-04-16 20:04:05 +0900

    [feat] 터렛의 회전 구현
    터렛의 회전까지 전부 구현하였다.

--- ProjectSetting/AdvancedTankMover.cs
+++ ProjectSetting/AdvancedTankMover.cs
@@ -7,10 +7,13 @@ public class AdvancedTankMover : MonoBehaviour
 {
     private float tankSpeed = 10;
     private float rotationSpeed = 20;
+    private float turretRotationSpeed = 30;
+    [SerializeField] GameObject turretPrefab;
 
     private void Update()
     {
         Move();
+        TurretMove();
     }
 
     private void Move()
@@ -38,6 +41,18 @@ public class AdvancedTankMover : MonoBehaviour
             transform.Translate(Vector3.left * tankSpeed * Time.deltaTime, Space.World);
             Quaternion vector3Rotation = Quaternion.LookRotation(Vector3.left);
             transform.rotation = Quaternion.Lerp(transform.rotation, vector3Rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void TurretMove()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            turretPrefab.transform.Rotate(Vector3.up, -turretRotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            turretPrefab.transform.Rotate(Vector3.up, turretRotationSpeed * Time.deltaTime);
         }
     }
 }
